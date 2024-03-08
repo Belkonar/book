b1b9654c917bfd3f6ef2e085b5917dbf85795798
Tom <JosephDotson@belkonar.com>
2024-03-07 23:19:54 -0500

    better, working build

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-pandoc $(ls draft/**/*.md) "--from=markdown-implicit_figures+rebase_relative_paths" -t pdf -s -o out/out.pdf
+mkdir -p out
+
+pandoc $(ls draft/**/*.md) "--from=markdown-implicit_figures+rebase_relative_paths" -t pdf -s -o out/book.pdf
+pandoc $(ls draft/**/*.md) "--from=markdown-implicit_figures+rebase_relative_paths" -t epub -s -o out/book.epub
