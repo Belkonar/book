defa1534996a95d6654391030bdadbb0b4b76068
Tom <JosephDotson@belkonar.com>
2024-03-08 20:30:58 -0500

    starting OOP chapter

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ mkdir -p out
 # the sort probs isn't needed but hey
 items=$(ls draft/**/*.md | sort)
 
-pandoc $items --metadata-file ./metadata.yaml "--from=markdown-implicit_figures+rebase_relative_paths" -t pdf -s -o out/book.pdf
-pandoc $items --metadata-file ./metadata.yaml "--from=markdown-implicit_figures+rebase_relative_paths" -t epub -s -o out/book.epub
+pandoc $items -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t pdf -s -o out/book.pdf
+pandoc $items -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t epub -s -o out/book.epub
