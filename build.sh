9f1ffbb79d520f9618b711032e39ad1a671d6529
Tom <JosephDotson@belkonar.com>
2024-03-23 19:21:16 -0400

    test with build command

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 
 mkdir -p out
 
-# the sort probs isn't needed but hey
-items=$(ls draft/**/*.md | sort)
-
-pandoc $items -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t pdf -s -o out/book.pdf
-pandoc $items -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t epub -s -o out/book.epub
+pandoc $(ls draft/**/*.md | sort) -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t pdf -s -o out/book.pdf
+pandoc $(ls draft/**/*.md | sort) -V documentclass=report --metadata-file ./metadata.yaml "--from=gfm-implicit_figures+rebase_relative_paths" -t epub -s -o out/book.epub
